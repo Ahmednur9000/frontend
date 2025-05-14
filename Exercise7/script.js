581b0c4d70f41eea8167fb82967edeb37e997126
Ahmednur Salaad Khaliff <ahmednur.khaliif0004@stud.hkr.se>
2025-05-14 13:18:20 +0200

    exercise 7

---
+++ Exercise7/script.js
@@ -0,0 +1,15 @@
+function greet () {
+    return "hello world";
+}
+
+console.log(greet());
+
+for (let i = 0; i < 5; i++) {
+
+}
+
+const person = {name:"Alex", age:30} 
+
+for (let key in person) {
+    console.log(key);
+}
